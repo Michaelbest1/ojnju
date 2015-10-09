49df9d2efe44d9e04284f8f5ddf44dfae49e9afb
Michaelbest1 <Michaelbest002@126.com>
2015-10-09 21:42:19 +0800

    add web

---
+++ web/index.php
@@ -0,0 +1,26 @@
+<?php
+	//$cache_time=1;
+	//require_once('./include/cache_start.php');
+    require_once("./include/db_info.inc.php");
+	require_once("./include/setlang.php");
+	$view_title= "LOGIN";
+
+	if (isset($_SESSION['user_id'])){
+		//echo "<a href=logout.php>Please logout First!</a>";
+		$targetURI="contest.php?user_id=".$_SESSION['user_id'];
+		header("Location: $targetURI");
+		//exit(1);
+	}
+	else {
+		$targetURI="loginpage.php";
+		header("Location: $targetURI");
+		//exit(1);
+	}
+
+/////////////////////////Template
+//require("template/".$OJ_TEMPLATE."/loginpage.php");
+/////////////////////////Common foot
+//if(file_exists('./include/cache_end.php'))
+//	require_once('./include/cache_end.php');
+?>
+
