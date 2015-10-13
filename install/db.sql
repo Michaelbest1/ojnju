350820ce7454f00961c0396b8a42eb626448d7cc
Michaelbest1 <Michaelbest002@126.com>
2015-10-13 22:07:16 +0800

    desc info

--- install/db.sql
+++ install/db.sql
@@ -130,6 +130,7 @@ CREATE TABLE  `solution` (
   `judgetime` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `pass_rate` DECIMAL(2,2) UNSIGNED NOT NULL DEFAULT 0,
   `lint_error` int UNSIGNED NOT NULL DEFAULT 0,
+  `desc` text,
   PRIMARY KEY (`solution_id`),
   KEY `uid` (`user_id`),
   KEY `pid` (`problem_id`),
